cfa0d8c15c0c90444c50a28c640affa4bcc5c9e2
Muhammad Bilal Akbar <mbilalakbarkh786@gmail.com>
2023-01-27 10:55:50 +0500

    Initial commit 27 January 2023

---
+++ README.md
@@ -0,0 +1,5 @@
+# Flutter UI - Login and Sign Up | With source code
+
+### Preview
+
+![App UI](/preview.png)
---
+++ android/app/src/main/kotlin/com/example/flutter_login_and_signup_ui/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutter_login_and_signup_ui
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
